36959cba7ab498c30d7106984d130708f7c5c3fa
zioutang <tangziou@gmail.com>
2017-07-24 16:47:39 -0700

    pull

--- reactApp/Components/ToolBar.js
+++ reactApp/Components/ToolBar.js
@@ -10,6 +10,9 @@ from 'draft-js';
 class ToolBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: 'format'
+    }
   }
 
   handleChange(event) {
@@ -21,10 +24,10 @@ class ToolBar extends React.Component {
     return (
       <div>
         <select value={this.state.value} onChange={this.handleChange}>
-            <option onClick={this.props.Bold} value="Bold">Grapefruit</option>
-            <option onClick={this.props.Italic} value="Italic">Lime</option>
-            <option onClick={this.props.Underline} value="cocUnderlineonut">Coconut</option>
-          </select>
+            <option onClick={this.props.Bold} value="Bold">Bold</option>
+            <option onClick={this.props.Italic} value="Italic">Italic</option>
+            <option onClick={this.props.Underline} value="Underline">Underline</option>
+        </select>
         {/* <button onClick={this.props.Bold}>Bold</button>
         <button onClick={this.props.Italic}>Italic</button>
         <button onClick={this.props.Underline}>Underline</button> */}

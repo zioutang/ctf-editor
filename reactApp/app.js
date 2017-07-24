9f9845c81ad4a7c6d81c118798daf381d2a4ce8f
zioutang <tangziou@gmail.com>
2017-07-24 14:05:29 -0700

    m1

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,52 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
+const React = require('react');
+const ReactDOM = require('react-dom');
+import {
+  Editor,
+  EditorState,
+}
+from 'draft-js';
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+    this.onChange = (editorState) => this.setState({
+      editorState
+    });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <Editor editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}/>
+      </div>
+    );
+  }
+}
+//
+ReactDOM.render(
+  <MyEditor/>, document.getElementById('root'));
+//
+// ReactDOM.render(
+//   <p>React lives!</p>, document.getElementById('root'));

61a5480ba5831d95e081307a19bdbd5fc76211ed
zioutang <tangziou@gmail.com>
2017-07-26 15:08:36 -0700

    m1

--- reactApp/Containers/DocEditor.js
+++ reactApp/Containers/DocEditor.js
@@ -58,8 +58,6 @@ class DocEditor extends React.Component {
   }
 
   onClick(...args) {
-    // console.log(this.state.editorState);
-
     if (!args[1]) {
       this.setState({
         editorState: RichUtils.toggleInlineStyle(this.state.editorState, args[0]),
@@ -94,7 +92,7 @@ class DocEditor extends React.Component {
     });
   }
   increaseSize() {
-    var newSize = this.state.currentFontSize + 1;
+    const newSize = this.state.currentFontSize + 1;
     let size = {
       [newSize]: {
         fontSize: `${newSize}px`
@@ -107,7 +105,7 @@ class DocEditor extends React.Component {
     });
   }
   decreaseSize() {
-    var newSize = this.state.currentFontSize - 1;
+    const newSize = this.state.currentFontSize - 1;
     let size = {
       [newSize]: {
         fontSize: `${newSize}px`
@@ -131,6 +129,7 @@ class DocEditor extends React.Component {
             colorHandle={this.formatColor}
             sizeIncrease={this.increaseSize}
             sizeDecrease={this.decreaseSize}
+            currentInlineStyle={this.state.editorState.getCurrentInlineStyle()}
           />
         </div>
         <div className="editor">
@@ -139,7 +138,7 @@ class DocEditor extends React.Component {
             blockRenderMap={blockTypes}
             customStyleMap={this.state.customStyleMap}
             editorState={this.state.editorState}
-            handleKeyCommand={this.handleKeyCommand.bind(this)}
+            handleKeyCommand={this.handleKeyCommand}
             onChange={this.onChange}/>
         </div>
         </div>

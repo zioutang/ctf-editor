6308577544d4595271dc2ad731d3e04d594a106b
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-26 11:56:47 -0700

    add signup and login pages

--- reactApp/app.js
+++ reactApp/app.js
@@ -19,25 +19,78 @@
 // ReactDOM.render(
 //   <MyEditor />, document.getElementById('root'));;
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+//
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <header>
+//         <nav>
+//           <ul>
+//             <li><Link to='/'>Login</Link></li>
+//             <li><Link to='/login'>Login</Link></li>
+//             <li><Link to='/register'>Register</Link></li>
+//           </ul>
+//         </nav>
+//       </header>
+//     )
+//   }
+// }
+//
+// class Home extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return(
+//       <div>this is home</div>
+//     )
+//   }
+// }
+//
+// class Main extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <main>
+//         <Switch>
+//           <Route exact path='/' component={Home}/>
+//           <Route exact path='/login' component={Login}/>
+//           <Route path='/register' component={Register}/>
+//         </Switch>
+//       </main>
+//     )
+//   }
+// }
+//
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Header/>
+//         <Main/>
+//       </div>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(
+//     <App />,
+//   document.getElementById('root'),
+// );
+
 import React from 'react';
-import { render } from 'react-dom';
-import BrowserRouter from 'react-router-dom';
-import App from './components/App';
+import ReactDom from 'react-dom';
+import HomePage from './Components/HomePage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    <div>
-      hello
-    <div>
-  }
-}
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDom.render(<HomePage />, document.getElementById('root'));
